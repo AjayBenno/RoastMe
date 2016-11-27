0951f443e2718c22ca382e8a689655d20dc4eb10
Ajay Benno <abenno@andrew.cmu.edu>
2016-11-26 19:21:25 -0500

    Updated for clearer instructions

--- roast.py
+++ roast.py
@@ -7,15 +7,16 @@ for line in fi:
 def lambda_handler(event,context):
     end = True
     if(event['request']['type'] == "IntentRequest"):
-        if(event['request']['type'] == "IntentRequest" and event['request']['intent']['name'] == "AMAZON.HelpIntent"):
+        if(event['request']['intent']['name'] == "AMAZON.HelpIntent"):
             ins = "Just ask alexa to roast you!"
             end = False
-        elif(event['request']['type'] == "IntentRequest" and event['request']['intent']['name'] == "AMAZON.StopIntent" or event['request']['intent']['name'] == "AMAZON.CancelIntent"):
+        elif(event['request']['intent']['name'] == "AMAZON.StopIntent" or event['request']['intent']['name'] == "AMAZON.CancelIntent"):
             ins = "Thanks for getting roasted, hope we never see you again!"
         else:
             ins = random.choice(insults) 
     elif(event['request']['type'] == "LaunchRequest"):
-        ins = "Welcome to Roast Me"
+        ins = "Welcome to Roast Me, ask me to roast you!"
+        end = False
     else:
         ins = "Goodbye"
     test = {}
